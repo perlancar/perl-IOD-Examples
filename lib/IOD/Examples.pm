ca852efade79703b40859cff5b00fa4017f16b04
perlancar (@pc-home) <perlancar@gmail.com>
2015-03-22 20:39:57 +0700

    [doc] Add description & See Also

--- lib/IOD/Examples.pm
+++ lib/IOD/Examples.pm
@@ -5,3 +5,15 @@ package IOD::Examples;
 
 1;
 # ABSTRACT: Example IOD configuration files
+
+=head1 DESCRIPTION
+
+This distribution contains various examples of L<IOD> configuration files in its
+per-dist share dir. It also serves as a spectest.
+
+
+=head1 SEE ALSO
+
+L<IOD>
+
+L<Config::IOD>, L<Config::IOD::Reader>
